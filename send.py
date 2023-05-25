d98e0f114fc65f1517fe58fb6f96e7fb46811a57
Arunashok <108691415+0hex7@users.noreply.github.com>
2023-05-26 01:32:22 +0530

    Update send.py

--- send.py
+++ send.py
@@ -38,18 +38,7 @@ with open(csv_file, 'r', encoding='UTF-8-sig') as file:
         body = f'''
 
 Dear Prof. {name},
-I hope this email finds you well. My name is Arun Ashok Badri, and I am currently a third-year undergraduate student pursuing a Bachelor's degree in CSE with a specialization in Cybersecurity at {home_univ}
-, India. I am writing to express my strong interest in obtaining a research internship opportunity at {univ}.
-I have been following the research and academic activities at {univ} with great admiration, and I am eager to join your esteemed institution to gain invaluable research experience in the domains of Cybersecurity, Artificial Intelligence (AI),Machine Learning (ML), Internet of Things (IOT), and Robotics.
-I am writing to request your guidance and approval in securing an internship at {univ} in your research lab. The labs I am interested to work for are,
-1. {lab1}
-2. {lab2}
-3. any other Cybersecurity/ Info sec related labs
-I am available for 3 to 6 months from September of this year.
-I have attached my Resume, Transcripts, Letter of Recommendations,and Statement of Purpose. If there are any specific application procedures or other documents that I need to submit, please provide me with the necessary information. I am prepared to complete any required application forms or provide additional supporting documents to strengthen my application.
-Thank you for considering my request, and I look forward to the possibility of joining {univ} as a research intern under your esteemed guidance.
-Yours sincerely,
-Arun Ashok Badri
+I hope this email finds you well. ...............
 {home_univ}
 '''
 
